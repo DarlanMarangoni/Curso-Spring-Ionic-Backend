2b9f3301d614f74b23b6c883fadf582f7e9fc08c
dmarangoni <marangoni.darlan1@gmail.com>
2018-09-03 18:11:15 -0300

    Validacao customizada de CPF ou CNPJ na insercao de cliente

--- src/main/java/com/dmarangoni/cursomc/resources/exception/FieldMessage.java
+++ src/main/java/com/dmarangoni/cursomc/resources/exception/FieldMessage.java
@@ -5,25 +5,25 @@ import java.io.Serializable;
 public class FieldMessage implements Serializable{
 	private static final long serialVersionUID = 1L;
 
-	private String fildName;
+	private String fieldName;
 	private String message;
 	
 	public FieldMessage() {
 		
 	}
 
-	public FieldMessage(String fildName, String message) {
+	public FieldMessage(String fieldName, String message) {
 		super();
-		this.fildName = fildName;
+		this.fieldName = fieldName;
 		this.message = message;
 	}
 
-	public String getFildName() {
-		return fildName;
+	public String getFieldName() {
+		return fieldName;
 	}
 
-	public void setFildName(String fildName) {
-		this.fildName = fildName;
+	public void setFieldName(String fildName) {
+		this.fieldName = fieldName;
 	}
 
 	public String getMessage() {
---
+++ src/main/java/com/dmarangoni/cursomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,39 @@
+package com.dmarangoni.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.dmarangoni.cursomc.domain.enums.TipoCliente;
+import com.dmarangoni.cursomc.dto.ClienteNewDTO;
+import com.dmarangoni.cursomc.resources.exception.FieldMessage;
+import com.dmarangoni.cursomc.services.validation.util.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+		
+		if(objDto.getTipoCliente().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+		}
+		
+		if(objDto.getTipoCliente().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		// inclua os testes aqui, inserindo erros na lista
+
+		for (FieldMessage e : list) {//acrescentando na lista nossos erros personalizados
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
