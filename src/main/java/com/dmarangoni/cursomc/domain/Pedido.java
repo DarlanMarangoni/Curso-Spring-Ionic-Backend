29ee3a22d3fef6c79e12f5c2129f2481f108778a
dmarangoni <marangoni.darlan1@gmail.com>
2018-09-11 17:08:09 -0300

    Implementando toString de Pedido

--- src/main/java/com/dmarangoni/cursomc/domain/Pedido.java
+++ src/main/java/com/dmarangoni/cursomc/domain/Pedido.java
@@ -1,8 +1,11 @@
 	package com.dmarangoni.cursomc.domain;
 
 import java.io.Serializable;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.Locale;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -132,4 +135,27 @@ public class Pedido implements Serializable{
 		return true;
 	}
 
+	@Override
+	public String toString() {
+		NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		StringBuilder builder = new StringBuilder();
+		builder.append("Pedido númemro: ");
+		builder.append(getId());
+		builder.append(", instante: ");
+		builder.append(sdf.format(getInstantDate()));
+		builder.append(", Cliente: ");
+		builder.append(getCliente().getNome());
+		builder.append(", Situação de pagamento: ");
+		builder.append(getPagamento().getEstado().getSituacao());
+		builder.append("\nDetalhes:\n");
+		for(ItemPedido ip: getItens()) {
+			builder.append(ip.toString());
+		}
+		builder.append("Valor Total: ");
+		builder.append(nf.format(getValorTotal()));
+		return builder.toString();
+	}
+
+	
 }
--- src/main/java/com/dmarangoni/cursomc/services/PedidoService.java
+++ src/main/java/com/dmarangoni/cursomc/services/PedidoService.java
@@ -33,6 +33,9 @@ public class PedidoService {
 	@Autowired
 	private ItemPedidoReposistory itemPedidoReposistory;
 	
+	@Autowired
+	private ClienteService clienteService;
+	
 	public Pedido find(int id) {		
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
@@ -41,6 +44,7 @@ public class PedidoService {
 
 	public Pedido insert(Pedido obj) {
 		obj.setInstantDate(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
 		obj.getPagamento().setEstadoPagamento(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 		if (obj.getPagamento() instanceof PagamentoComBoleto) {
@@ -51,10 +55,12 @@ public class PedidoService {
 		pagamentoRepository.save(obj.getPagamento());
 		for(ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
-			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
 			ip.setPedido(obj);
 		}
 		itemPedidoReposistory.saveAll((obj.getItens()));
+		System.out.println(obj);
 		return obj;
 	}
 	
