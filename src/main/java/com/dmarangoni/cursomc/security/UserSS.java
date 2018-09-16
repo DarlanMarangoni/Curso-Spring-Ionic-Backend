130dce27c3789ab3f082540f18ed2ca5a905fa83
dmarangoni <marangoni.darlan1@gmail.com>
2018-09-16 11:33:00 -0300

    Implementando autenticacao e geracao do token JWT

---
+++ src/main/java/com/dmarangoni/cursomc/security/UserSS.java
@@ -0,0 +1,77 @@
+package com.dmarangoni.cursomc.security;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.dmarangoni.cursomc.domain.enums.Perfil;
+
+public class UserSS implements UserDetails{
+
+	private int id;
+	private String email;
+	private String senha;
+	private Collection<? extends GrantedAuthority> authorities;
+	
+	public UserSS() {
+		
+	}	
+	
+	public UserSS(int id, String email, String senha, Set<Perfil> perfis) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.senha = senha;
+		//converte a lista de perfil para collection pedida pelo spring
+		this.authorities = perfis.stream().map(x -> new SimpleGrantedAuthority(x.getDescricao())).collect(Collectors.toList());
+	}
+
+
+	public int getId() {
+		return id;
+	}
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return authorities;
+	}
+
+	@Override
+	public String getPassword() {
+		return senha;
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
