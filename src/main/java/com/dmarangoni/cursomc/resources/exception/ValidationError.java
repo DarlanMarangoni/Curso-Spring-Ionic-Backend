3b6f333b76da929cc0882c4171d5659cb71f6f17
dmarangoni <marangoni.darlan1@gmail.com>
2018-10-01 18:55:21 -0300

    Padronizando formato das excessoes

--- src/main/java/com/dmarangoni/cursomc/resources/exception/ValidationError.java
+++ src/main/java/com/dmarangoni/cursomc/resources/exception/ValidationError.java
@@ -6,10 +6,10 @@ import java.util.List;
 public class ValidationError extends StandardError{
 	private static final long serialVersionUID = 1L;
 
-	private List<FieldMessage> errors = new ArrayList<>();
-	
-	public ValidationError(Integer status, String msg, long timeStamp) {
-		super(status, msg, timeStamp);		
+	private List<FieldMessage> errors = new ArrayList<>();	
+
+	public ValidationError(long timeStamp, Integer status, String error, String message, String path) {
+		super(timeStamp, status, error, message, path);
 	}
 
 	public List<FieldMessage> getErrors() {
